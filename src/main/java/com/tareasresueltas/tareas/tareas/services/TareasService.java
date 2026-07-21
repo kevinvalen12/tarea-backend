70c77ab0292cacd498da240ba44dac5a5087bc82
kevinval12 <valenciakevin894@gmail.com>
2026-07-20 22:06:32 -0500

    Docker Configuration

--- src/main/java/com/tareasresueltas/tareas/tareas/services/TareasService.java
+++ src/main/java/com/tareasresueltas/tareas/tareas/services/TareasService.java
@@ -1,5 +1,10 @@
 package com.tareasresueltas.tareas.tareas.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TareasService {
-    
+    public String tareas() {
+        return "";
+    }
 }
